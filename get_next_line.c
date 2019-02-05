1add5dccc958938c33cf5a1a97ee3409aa29992e
Lola ROTA <grota@e3r7p23.42.fr>
2019-02-05 19:16:11 +0100

    fuck la moulinette, bordel

--- get_next_line.c
+++ get_next_line.c
@@ -6,123 +6,60 @@
 /*   By: grota <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/18 12:51:53 by grota             #+#    #+#             */
-/*   Updated: 2019/02/05 14:40:44 by grota            ###   ########.fr       */
+/*   Updated: 2019/02/05 17:15:44 by grota            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static int		ft_buf_manip(char **line, t_gnl *gnl, char **buf, unsigned int flag)
+static int		ft_buf_manip(char **line, char **next,
+												char **buf, unsigned int flag)
 {
 	if (flag & 1)
 	{
 		(void)buf;
-		if (!(gnl->line) || !ft_isinstr(gnl->line, '\n'))
+		if (!next || !ft_isinstr(*next, '\n'))
 			return (0);
-		*line = ft_strsubf(gnl->line, 0, ft_strclen(gnl->line, '\n'));
-		gnl->line = ft_strsubf(gnl->line, ft_strclen(gnl->line, '\n') + 1, ft_strlen(gnl->line));
+		*line = ft_strsub(*next, 0, ft_strclen(*next, '\n'));
+		*next = ft_strsub(*next, ft_strclen(*next, '\n') + 1,
+													ft_strlen(*next));
 		return (1);
 	}
 	else if (flag & 2)
 	{
-		*line = ft_strjoinf(gnl->line, ft_strsub(*buf, 0, ft_strclen(*buf, '\n')), 2);
-		gnl->line = ft_strsubf(*buf, ft_strclen(*buf, '\n') + 1, BUFF_SIZE + 1);
+		*line = ft_strjoinf(*next, ft_strsub(*buf, 0,
+												ft_strclen(*buf, '\n')), 3);
+		*next = ft_strsubf(*buf, ft_strclen(*buf, '\n') + 1,
+								BUFF_SIZE + 1);
 		return (1);
 	}
 	return (1);
 }
 
-static t_gnl	*ft_newlink(const int fd, t_gnl *gnl)
-{
-	if (!gnl)
-	{
-		if (!(gnl = (t_gnl *)malloc(sizeof(*gnl))))
-			return (NULL);
-		gnl->first = gnl;
-	}
-	else
-	{
-		while (gnl->next && gnl->fd != fd)
-			gnl = gnl->next;
-		if (gnl->fd == fd)
-			return (gnl);
-		if (!(gnl->next = (t_gnl *)malloc(sizeof(*gnl))))
-			return (NULL);
-		gnl->next->first = gnl->first;
-		gnl = gnl->next;
-	}
-	gnl->fd = fd;
-	gnl->line = NULL;
-	gnl->next = NULL;
-	return (gnl);
-}
-
-static t_gnl	*ft_select_fd(const int fd, t_gnl *gnl)
-{
-	if (!(gnl = ft_newlink(fd, gnl)))
-		return (0);
-	gnl = gnl->first;
-	while (fd != gnl->fd && gnl->next)
-		gnl = gnl->next;
-	if (fd != gnl->fd && gnl->next == NULL)
-		if (ft_newlink(fd, gnl) == 0)
-			return (0);
-	return (gnl);
-}
-
 int				get_next_line(const int fd, char **line)
 {
-	static t_gnl	*gnl = NULL;
+	static char		*gnl[4865];
 	int				ret;
 	char			*buf;
 
 	if (fd < 0 || BUFF_SIZE < 1 || !line || !(buf = ft_strnew(BUFF_SIZE)))
 		return (-1);
-	if (!(gnl = ft_select_fd(fd, gnl)))
-		return (-1);
-	if (ft_strlen(gnl->line) && ft_buf_manip(line, gnl, &buf, 1))
+	if (ft_strlen(gnl[fd]) && ft_buf_manip(line, gnl + fd, &buf, 1))
 		return (1);
 	while ((ret = read(fd, buf, BUFF_SIZE)) && ret > 0)
 	{
 		if (ft_isinstr(buf, '\n'))
-		{
-			return (ft_buf_manip(line, gnl, &buf, 2));
-		}
-		gnl->line = ft_strjoinf(gnl->line, buf, 3);
+			return (ft_buf_manip(line, gnl + fd, &buf, 2));
+		gnl[fd] = ft_strjoinf(gnl[fd], buf, 3);
 		buf = ft_strnew(BUFF_SIZE);
 	}
-	if (ft_strlen(gnl->line))
+	if (ft_strlen(gnl[fd]))
 	{
-		*line = gnl->line;
-		gnl->line = NULL;
+		*line = gnl[fd];
+		gnl[fd] = NULL;
 		return (1);
 	}
 	if (!ret)
-	{
 		*line = NULL;
-		free(gnl->line);
-	}
 	return (ret);
-}
-
-int	main(int argc, char **argv)
-{
-	int	i = 1;
-	int	fd_1 = 0;
-	int	fd_2 = 0;
-	char	*line;
-
-	line = ft_strnew(12);
-	if (argc >= 2)
-		fd_1 = open(argv[1], O_RDONLY);
-	if (argc == 3)
-		fd_2 = open(argv[2], O_RDONLY);
-	while ((i = get_next_line(fd_1, &line)))
-	{
-		ft_putendl(line);
-		i = get_next_line(fd_2, &line);
-		ft_putendl(line);
-	}
-	read(0, O_RDONLY, BUFF_SIZE);
-	return (1);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,24 +6,16 @@
 /*   By: grota <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/18 12:51:52 by grota             #+#    #+#             */
-/*   Updated: 2019/02/05 14:03:43 by grota            ###   ########.fr       */
+/*   Updated: 2019/02/05 16:48:05 by grota            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 4
+# define BUFF_SIZE 75
 
 # include "libft/libft.h"
-
-typedef struct		s_gnl
-{
-	int				fd;
-	char			*line;
-	struct s_gnl	*next;
-	struct s_gnl	*first;
-}					t_gnl;
 
 int					get_next_line(const int fd, char **line);
 
